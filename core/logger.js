c63d76dfaeed5cee92e61cdd374615ed38f5a09d
myfCode <543830959@qq.com>
2018-02-03 08:43:56 +0800

    Update logger.js

--- core/logger.js
+++ core/logger.js
@@ -6,7 +6,7 @@ var logger = function(info){
     var today = new Date();
     var loggerPath = path.resolve(__dirname, '../logger') ;
     var filename = loggerPath + '/' + today.format('yyyy-MM-dd') + '.log';
-    console.log(filename+'-----------')
+   
     fs.appendFile(filename, today.format('yyyy-MM-dd hh:mm:ss') 
         + '\n' + info + '\n\n')
 }
